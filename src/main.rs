d8cbd4731990f25941b9a7dd5e011250107d563e
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2021-11-04 18:19:46 -0400

    add few other route to debug

--- src/main.rs
+++ src/main.rs
@@ -42,11 +42,12 @@ async fn main() -> std::io::Result<()> {
             )
             .service(
                 web::scope("/auth")
-                    .route("/tes", web::get().to(|| HttpResponse::Ok()))
+                    .service(web::resource("/tes").route(web::get().to(|| HttpResponse::Gone())))
+                    .service(web::resource("/register").route(web::get().to(|| HttpResponse::Gone())))
                     .service(web::resource("/register").route(web::post().to(register_handler)))
                     .service(web::resource("/login").route(web::post().to(login_handler))),
             )
-            .route("/tes", web::get().to(|| HttpResponse::Ok()))
+            .route("/tes", web::get().to(|| HttpResponse::Gone()))
     })
     .bind("127.0.0.1:8080")?
     .run()
